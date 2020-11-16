ada48bc9dd1cd6bf0a986bbf61a298625d2d819e
Nicario546 <73836749+Nicario546@users.noreply.github.com>
2020-11-16 17:43:09 +0300

    Update sketch_nov09a.ino

--- sketch_nov09a.ino
+++ sketch_nov09a.ino
@@ -5,13 +5,11 @@
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
-Servo servo;
+char buffer [5];
 void setup() {
   Serial.begin(9600);
   SPI.begin();  //  инициализация SPI / Init SPI bus.
   mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
-  servo.attach(6);
-  servo.write(0);  // устанавливаем серву в закрытое сосотояние
 }
 void loop() {
   // Поиск новой метки
@@ -32,17 +30,19 @@ void loop() {
  
   if (uidDec == 2841136825) // Сравниваем Uid метки, если он равен заданому то серва открывает.
   {
-    Serial.println("Сикорский Артем пришол");
-    servo.write(90); // Поворациваем серву на угол 90 градусов(Отпираем какой либо механизм: задвижку, поворациваем ключ и т.д.)
-    delay(3000); // пауза 3 сек и механизм запирается.
-    servo.write(0);  // устанавливаем серву в закрытое сосотояние
+    int value =1;
+    Serial.print("#S|TEST|[");
+    Serial.print(itoa((value), buffer, 10));
+    Serial.println("]#");
+    delay (10000); 
   }
 
     if (uidDec == 1773590713) // Сравниваем Uid метки, если он равен заданому то серва открывает.
   {
-    Serial.println("Олег Игоревич пришол");
-    servo.write(90); // Поворациваем серву на угол 90 градусов(Отпираем какой либо механизм: задвижку, поворациваем ключ и т.д.)
-    delay(3000); // пауза 3 сек и механизм запирается.
-    servo.write(0);  // устанавливаем серву в закрытое сосотояние
+    int value =2;
+    Serial.print("#S|TEST|[");
+    Serial.print(itoa((value), buffer, 10));
+    Serial.println("]#");
+    delay (10000); 
   }
 }
