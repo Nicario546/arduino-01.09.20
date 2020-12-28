c4fe7c57f9bcc23dcd430592f872fc72a0695187
Nicario546 <73836749+Nicario546@users.noreply.github.com>
2020-12-28 18:10:45 +0300

    Add files via upload

---
+++ sketch_dec28a/sketch_dec28a.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include <SoftwareSerial.h>
+#include <MFRC522.h> // библиотека "RFID".
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+unsigned long uidDec, uidDecTemp;  // для храниения номера метки в десятичном формате
+char buffer [5];
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();  //  инициализация SPI / Init SPI bus.
+  mfrc522.PCD_Init();     // инициализация MFRC522 / Init MFRC522 card.
+}
+void loop() {
+  // Поиск новой метки
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+  // Выбор метки
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+  uidDec = 0;
+  // Выдача серийного номера метки.
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    uidDecTemp = mfrc522.uid.uidByte[i];
+    uidDec = uidDec * 256 + uidDecTemp;
+  }
+ 
+  if (uidDec == 2841136825) // Сравниваем Uid метки, если он равен заданому то серва открывает.
+  {
+    int value =1;
+    Serial.print("#S|TEST|[");
+    Serial.print(itoa((value), buffer, 10));
+    Serial.println("]#");
+    delay (10000); 
+  }
+
+    if (uidDec == 1773590713) // Сравниваем Uid метки, если он равен заданому то серва открывает.
+  {
+    int value =2;
+    Serial.print("#S|TEST|[");
+    Serial.print(itoa((value), buffer, 10));
+    Serial.println("]#");
+    delay (10000); 
+  }
+}
